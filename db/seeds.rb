822faff8c20bafda4adf3a267789f4f28b1fa896
Nadia Isela Angulo Martinez de la Vega <nayita7_5@hotmail.com>
2022-05-23 17:53:29 -0500

    Create seed with 1 user and dog

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create!(
+  email: "user@owner.com",
+  password: "123456",
+  name: "Max",
+  phone_number: "476654878",
+  address: "Ajhasjdh akshd 45"
+)
+
+Dog.create!(
+  name: "Max",
+  breed: "German shepherd",
+  born_on: rand(Date.new(2012, 1, 1)..Date.today),
+  sex: ["male", "female"].sample,
+  neutered: [true, false].sample,
+  size: %w[small medium large giant],
+  user: user
+)
