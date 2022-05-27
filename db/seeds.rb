b7bc9fdf0ecf8530e647246d009fadf6fa88598a
jose garcia <joey.garcia3711@gmail.com>
2022-05-27 14:16:52 -0500

    finished geocoder seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,24 @@
-10.times do |n|
+dog_attributes = [
+  { breed: "Yorkshire", size: "small", image_url: "app/assets/images/york.jpg" },
+  { breed: "San Bernard", size: "giant", image_url: "app/assets/images/san_bernanrd.jpg" },
+  { breed: "Rat Terrier", size: "small", image_url: "app/assets/images/jujuba.jpg" },
+  { breed: "German Shepherd", size: "large", image_url: "app/assets/images/german_shepherd.jpg" },
+  { breed: "chihuahua", size: "small", image_url: "app/assets/images/chihuahua.jpg" },
+  { breed: "Golden Retriever", size: "large", image_url: "app/assets/images/golden_retriever.jpg" },
+]
+dog_attributes_index = -1
+
+working_addresses = [
+  "Xola 958, Narvarte Poniente - CDMX",
+  "Avenida Alvaro Obregon 213, Roma Norte - CDMX",
+  "Avenida Alvaro Obregon 52, Roma Norte - CDMX",
+  "Avenida Chapultepec 6, 06600, CDMX",
+  "Avenida Chapultepec 8, 06600, CDMX",
+  "Avenida Chapultepec 12, Juarez, CDMX",
+]
+working_addresses_index = -1
+
+dog_attributes.size.times do |n|
   puts "Generating user #{n + 1}"
   user = User.create!(
     email: "user#{n + 1}@owner.com",
@@ -8,16 +28,21 @@
     address: Faker::Address.street_address
   )
 
-  rand(1..3).times do
-    puts "Generating dog for user #{n + 1}"
-    Dog.create!(
+  puts "Generating dog for user #{n + 1}"
+  dog_info = dog_attributes[(dog_attributes_index += 1) % dog_attributes.size]
+  dog = Dog.new(
+    dog_info.except(:image_url).merge(
       name: Faker::Creature::Dog.name,
-      breed: Faker::Creature::Dog.breed,
       born_on: rand(Date.new(2012, 1, 1)..Date.today),
       sex: ["male", "female"].sample,
       neutered: [true, false].sample,
-      size: %w[small medium large giant].sample,
-      user: user
+      user: user,
+      address: working_addresses[(working_addresses_index += 1) % working_addresses.size]
     )
-  end
+  )
+  # dog attach image
+  file = File.open(dog_info[:image_url])
+  dog.photo.attach(io: file, filename: "#{dog.name}-#{dog_info[:breed].downcase}.jpg", content_type: 'image/png')
+
+  dog.save!
 end
