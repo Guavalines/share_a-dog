f8dd100eb5c84f286e508bfd92e2850102dd8951
guavalines <konstrictor@gmail.com>
2023-02-26 23:28:03 -0800

    finished editing seedfile pics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
 dog_attributes = [
-  { breed: "Yorkshire", size: "small", image_url: "app/assets/images/york.jpg" },
-  { breed: "San Bernard", size: "giant", image_url: "app/assets/images/san_bernanrd.jpg" },
-  { breed: "Rat Terrier", size: "small", image_url: "app/assets/images/jujuba.jpg" },
-  { breed: "German Shepherd", size: "large", image_url: "app/assets/images/german_shepherd.jpg" },
-  { breed: "chihuahua", size: "small", image_url: "app/assets/images/chihuahua.jpg" },
-  { breed: "Golden Retriever", size: "large", image_url: "app/assets/images/golden_retriever.jpg" },
+  { breed: "Bull Terrier", size: "medium", image_url: "app/assets/images/Bull_Terrier.jpeg" },
+  { breed: "Chihuahua", size: "small", image_url: "app/assets/images/Chihuahua.jpg" },
+  { breed: "Great Dane", size: "giant", image_url: "app/assets/images/Great_Dane.jpeg" },
+  { breed: "Komondor", size: "giant", image_url: "app/assets/images/Komondor.jpeg" },
+  { breed: "Pit Bull", size: "large", image_url: "app/assets/images/Pitt_Bull.jpeg" },
+  { breed: "Poodle", size: "medium", image_url: "app/assets/images/Poodle.jpeg" },
+  { breed: "Shih Tzu", size: "small", image_url: "app/assets/images/Shih_Tzu.jpeg" },
+  { breed: "Siberian Husky", size: "large", image_url: "app/assets/images/Siberian_Husky.jpeg" },
 ]
 dog_attributes_index = -1
 
@@ -15,6 +17,8 @@ working_addresses = [
   "Avenida Chapultepec 6, 06600, CDMX",
   "Avenida Chapultepec 8, 06600, CDMX",
   "Avenida Chapultepec 12, Juarez, CDMX",
+  "Avenida Chapultepec 13, 06600, CDMX",
+  "Avenida Alvaro Obregon 40, Roma Norte - CDMX",
 ]
 working_addresses_index = -1
 
